df0042bf3f5223588915de1fc5276f89067cc308
egwilborn <egwilborn@gmail.com>
2023-09-15 17:54:06 -0500

    annotations examples

---
+++ types.ts
@@ -0,0 +1,8 @@
+const today = new Date();
+today.getMonth();
+const person = {
+  age: 20,
+};
+//typescript defaults to the types and properties the object has since we didn't create a new type
+class Color {}
+const red = new Color();
